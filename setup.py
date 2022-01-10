a89d12e46011a4464037b11e82a4b6ec679329f1
César Richard <cesar.richard@weezevent.com>
2022-01-11 00:46:28 +0100

    Updated setup infos

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/SiMDE-Projects/oauth_pda_utc/issues",
+    },
+)
