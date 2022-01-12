ded1fda0d3c3f35ba68aebfddd2128961eafc3f2
César Richard <cesar.richard@weezevent.com>
2022-01-12 18:48:50 +0100

    :-)

--- oauth_pda_app/decorators.py
+++ oauth_pda_app/decorators.py
@@ -1,16 +1,14 @@
-from requests import Response
+from django.http import HttpResponse
 
 
 def require_sso_authenticated(func):
     def wrapper(self, request, *args, **kwargs):
-        if 'token' not in request.session.keys():
-            res = Response()
-            res.status_code = 401
-            res.reason = 'Unauthorized'
-            return res
+        print(request.session)
+        if not request.session or 'token' not in request.session.keys():
+            return HttpResponse('Unauthorized', status=401)
 
         token = request.session['token']
-        request.headers['Authorization'] = 'Bearer {}'.format(token)
+        request.META['HTTP_AUTHORIZATION'] = 'Bearer {}'.format(token)
 
         return func(self, request, *args, **kwargs)
 
